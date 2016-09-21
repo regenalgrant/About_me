9676fc9f373091d36fd813d1d745f596304c45e4
regenalgrant <regenal@mac.com>
2016-09-20 21:13:50 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+var username = prompt('What is your name?');
+alert('Welcome, ' + username + '!')
+console.log('The user\'s name is ' + username);
+
+ var age = prompt('How old are you?');
+alert('Wow, ' + age + '! ' + username);
+console.log('The user\'s you are ' + age);
+
+ var height = prompt('How tall are you?');
+alert('Hmmm!, ' + height + ' are you sure! ');
+console.log('The user\'s height ' + height);
+
+ var reside = prompt('Where do you reside?');
+alert('Let me goggle that!, ' + username + '! ' + 'it sucks to be you!');
+console.log('The user\'s you live ' + username);
+};
