4202d017826bdb5e639397974655ee1e239c08d3
regenalgrant <regenal@mac.com>
2016-09-21 13:51:31 -0700

    change2 new

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+'use strict';
+var user = prompt ('I am Regenal') {
 var username = prompt('What is your name?');
-alert('Welcome, ' + username + '!')
-console.log('The user\'s name is ' + username);
+  alert('Welcome, ' + username + '!');
+  console.log('The user\'s name is ' + username);
+}
 
- var age = prompt('How old are you?');
-alert('Wow, ' + age + '! ' + username);
-console.log('The user\'s you are ' + age);
+var age = prompt('How old are you?') {
+  alert('Wow, ' + age + '! ' + username);
+  console.log('The user\'s you are ' + age);
+}
 
- var height = prompt('How tall are you?');
-alert('Hmmm!, ' + height + ' are you sure! ');
-console.log('The user\'s height ' + height);
+var height = prompt('How tall are you?') {
+  alert('Hmmm!, ' + height + ' are you sure! ');
+  console.log('The user\'s height ' + height);
+}
 
- var reside = prompt('Where do you reside?');
-alert('Let me goggle that!, ' + username + '! ' + 'it sucks to be you!');
-console.log('The user\'s you live ' + username);
-};
+var reside = prompt('Where do you reside?') {
+  alert('Let me goggle that!, ' + username + '! ' + 'it sucks to be you!');
+  console.log('The user\'s you live ' + username);
+}
+
+var reside = prompt('Am I good looking?'); {
+	alert('I know!, ' + username + '! ' + 'it sucks to be you!');
+		console.log('The user\'s you live ' + username);
+}
